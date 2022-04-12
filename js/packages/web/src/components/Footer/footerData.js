c4e87ba7b66c547a5f4bffbe30fff816995dd5ae
Fyfy Dev <101877327+streamdao@users.noreply.github.com>
2022-04-12 11:50:18 +0300

    Fist Commit

---
+++ js/packages/web/src/components/Footer/footerData.js
@@ -0,0 +1,54 @@
+export const footerConf = {
+    showShopName: true,
+    showEmailSubscriber: true,
+    emailSubscriberText: 'Join our mailing list for updates about our artists and more.',
+    components: [{
+            title: 'Company',
+            links: [{
+                    label: 'About',
+                    url: 'https://streamdao.org',
+                },
+                {
+                    label: 'Terms of service',
+                    url: 'https://streamdao.org',
+                },
+            ],
+        },
+        {
+            title: 'Help',
+            links: [{
+                    label: 'FAQ',
+                    url: 'https://marketplace.streamprotocol.org/faq',
+                },
+                {
+                    label: 'Support',
+                    url: 'https://marketplace.streamprotocol.org/support',
+                },
+                {
+                    label: 'Privacy policy',
+                    url: 'https://marketplace.streamprotocol.org/privacy-policy',
+                },
+                {
+                    label: 'Your purchases',
+                    url: 'https://marketplace.streamprotocol.org/your-purchases',
+                },
+            ],
+        },
+        {
+            title: 'Follow us',
+            links: [{
+                    label: 'Instagram',
+                    url: 'www.instagram.com',
+                },
+                {
+                    label: 'Twitter',
+                    url: 'https://twitter.com/dao_stream/',
+                },
+                {
+                    label: 'Discord',
+                    url: 'www.discord.com',
+                },
+            ],
+        },
+    ],
+};
